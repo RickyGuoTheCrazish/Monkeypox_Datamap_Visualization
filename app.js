3bfc2d2db85a6b7d1cdc99f9a85224f4881cd0d2
Ricky Guo <rickyguo@Rickys-MacBook-Air.local>
2022-09-14 23:50:03 +1000

    v1.02

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // app.js
+require("dotenv").config()
+const path = require('path');
 
 const express = require('express');
 const connectDB = require('./config/db');
@@ -6,13 +8,17 @@ const connectDB = require('./config/db');
 
 const app = express();
 
+const port = process.env.PORT || 8000;
+const publicPath = path.join(__dirname, '..', 'public');
 
 
 connectDB();
 
-app.get('/', (req, res) => res.send('Hello world!'));
 
-const port = process.env.PORT || 8082;
+app.use(express.static(path.join(__dirname, "client", "build")))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
