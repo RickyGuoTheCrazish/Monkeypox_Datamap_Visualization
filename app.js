db1faa0db66caadb42ada16618e1dadf5a3f3e52
Ricky Guo <rickyguo@Rickys-MacBook-Air.local>
2022-09-15 00:35:14 +1000

    v1.03

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 // app.js
-require("dotenv").config()
+const dotenv = require('dotenv');  
 const path = require('path');
 
 const express = require('express');
-const connectDB = require('./config/db');
-
+// const connectDB = require('./config/db');
+const mongoose = require("mongoose");
 
 const app = express();
 
 const port = process.env.PORT || 8000;
 const publicPath = path.join(__dirname, '..', 'public');
-
-
-connectDB();
+dotenv.config(); 
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+// connectDB();
 
 
 app.use(express.static(path.join(__dirname, "client", "build")))
